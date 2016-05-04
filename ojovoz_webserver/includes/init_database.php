13f032a445463c25dcadbb2778564b7fb48f7ac7
ojovoz <cubo23@yahoo.com>
2016-05-04 16:51:10 +0200

    Revert "Update init_database.php"
    This reverts commit 7ef72192ed4ad01123fe1a988c5b76b36a043838.

--- ojovoz_webserver/includes/init_database.php
+++ ojovoz_webserver/includes/init_database.php
@@ -3,9 +3,9 @@
 //initialize dbConnection
 function initDB() {
 	$host="localhost";
-	$db="mydatabase";
-	$db_user="db_user";
-	$db_pass="db_pass";
+	$db="ovwebserver";
+	$db_user="mysautiyaw";
+	$db_pass="j8bA0y11";
 	$dbh=mysql_connect ($host, $db_user, $db_pass);
 	mysql_select_db ($db);
 	return $dbh;
