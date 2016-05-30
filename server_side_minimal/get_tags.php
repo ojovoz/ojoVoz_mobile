f822995786383ee397149b007900099558545410
ojovoz <cubo23@yahoo.com>
2016-05-30 15:29:24 +0200

    Major changes in project structure

--- server_side_minimal/get_tags.php
+++
@@ -1,9 +0,0 @@
-<?
-//this script returns a list of tags for a specific phone ID
-//tags are separated by ';'
-if(isset($_GET['id'])) {
-	if ($_GET['id'] == 'test') {
-		echo('tag1;tag2;tag3');
-	}
-}
-?>
